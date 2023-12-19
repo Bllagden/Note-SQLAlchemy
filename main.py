b6ccf7e6c40e5471444aa493f640457675cdd32a
Bllagden <bllagden99@gmail.com>
2023-12-19 20:04:24 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.insert(1, os.path.join(sys.path[0], "src"))
+# C:\GLOBAL_PATH_TO_PROJECT\root\src   (добавление src в список путей на 2 место)
+# for i in sys.path: print(i)
+
+if __name__ == "__main__":
+    pass
